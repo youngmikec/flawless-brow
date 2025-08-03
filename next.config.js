29c63dd9ffee6db4ced0dec6d9e167c8359f1303
youngmikec <michaelozor15@gmail.com>
2025-08-03 08:15:18 +0100

    disabled linting in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  eslint: {
+    ignoreDuringBuilds: true,
+    // dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 };
 
 module.exports = nextConfig;
